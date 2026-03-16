76a9b12d3c9181be07042dc960d5279618ced368
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2026-03-16 12:07:17 -0500

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,78 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			title: 'Soil',
-			template: './src/index.html',
-			filename: './index.html',
-			favicon: './src/img/logo.png',
-		}),
-		new CopyWebpackPlugin({
-			patterns: [
-				{ from: './src/manifest.json', to: 'manifest.json' },
-				{ from: './src/service-worker.js', to: 'service-worker.js' },
-			],
-		}),
-	],
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: './',
-		host: '0.0.0.0',
-		hot: true,
-		watchFiles: ['src/**/*', 'src/soil.csv'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					// Creates `style` nodes from JS strings
-					'style-loader',
-					// Translates CSS into CommonJS
-					'css-loader',
-					// Compiles Sass to CSS
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: ['file-loader'],
-			},
-			{
-				test: /\.csv$/,
-				loader: 'csv-loader',
-				options: {
-					dynamicTyping: true,
-					header: true,
-					skipEmptyLines: true,
-				},
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-		],
-	},
-}
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Soil",
+      template: "./src/index.html",
+      filename: "./index.html",
+      favicon: "./src/img/favicon.png",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/manifest.json", to: "manifest.json" },
+        { from: "./src/service-worker.js", to: "service-worker.js" },
+      ],
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./",
+    host: "0.0.0.0",
+    hot: true,
+    watchFiles: ["src/**/*", "src/soil.csv"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.csv$/,
+        loader: "csv-loader",
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
